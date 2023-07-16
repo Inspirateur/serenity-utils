52a39a38509f9e0eddbd4773ce78a08dab5580aa
Teo <teo.orthlieb@gmail.com>
2023-07-16 11:04:53 +0200

    way better api for db map

--- src/dbmap.rs
+++ src/dbmap.rs
@@ -1,29 +1,21 @@
 use std::marker::PhantomData;
-use rusqlite::Connection;
+use rusqlite::{Connection, params, ToSql, types::FromSql};
 use anyhow::Result;
 
 #[derive(Debug, Clone)]
-pub struct DBMap<K: ToString + TryFrom<String>, V: ToString + TryFrom<String>>
-where
-    anyhow::Error: From<<K as TryFrom<String>>::Error>, 
-    anyhow::Error: From<<V as TryFrom<String>>::Error>
-{
+pub struct DBMap<K: ToSql + FromSql, V: ToSql + FromSql> {
     path: String,
-    key_type: PhantomData<K>,
-    value_type: PhantomData<V>
+    key_type: PhantomData<*const K>,
+    value_type: PhantomData<*const V>
 }
 
-impl<K: ToString + TryFrom<String>, V: ToString + TryFrom<String>> DBMap<K, V>
-where
-    anyhow::Error: From<<K as TryFrom<String>>::Error>, 
-    anyhow::Error: From<<V as TryFrom<String>>::Error>
-{
+impl<K: ToSql + FromSql, V: ToSql + FromSql> DBMap<K, V> {
     pub fn new(db_path: &str) -> Result<Self> {
         let conn = Connection::open(db_path)?;
         conn.execute(
             "CREATE TABLE IF NOT EXISTS Map (
-                key TEXT PRIMARY KEY,
-                value TEXT
+                key BLOB PRIMARY KEY,
+                value BLOB
             )",
             [],
         )?;
@@ -36,41 +28,36 @@ where
 
     pub fn insert(&self, key: K, value: V) -> Result<()> {
         let conn = Connection::open(&self.path)?;
-        let key = key.to_string();
-        let value = value.to_string();
         conn.execute(
             "INSERT OR REPLACE INTO Map (key, value)
             VALUES (?1, ?2)", 
-            [key, value]
+            params![key, value]
         )?;
         Ok(())
     }
 
     pub fn get(&self, key: K) -> Result<V> {
         let conn = Connection::open(&self.path)?;
-        let key = key.to_string();
         let mut stmt = conn
             .prepare("SELECT value FROM Map WHERE key = ?1")
             .unwrap();
         let value = stmt
             .query_row([key], |row| {
-                row.get::<usize, String>(0)
-            })?.try_into()?;
-        
+                row.get::<usize, V>(0)
+            })?;
         Ok(value)
     }
 
     pub fn get_keys(&self, value: V) -> Result<Vec<K>> {
         let conn = Connection::open(&self.path)?;
-        let value = value.to_string();
         let mut stmt = conn
             .prepare("SELECT key FROM Map WHERE value = ?1")
             .unwrap();
         let values = stmt
             .query_map([value], |row| {
-                row.get::<usize, String>(0)
+                row.get::<usize, K>(0)
             })?
-            .collect::<Result<Vec<String>, rusqlite::Error>>()?;
-        Ok(values.into_iter().map(K::try_from).collect::<Result<Vec<_>, _>>()?)
+            .collect::<Result<Vec<K>, rusqlite::Error>>()?;
+        Ok(values)
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,18 @@ mod dbmap;
 pub use dbmap::*;
 pub use bot::*;
 pub use utils::*;
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+
+    #[test]
+    fn it_works() {
+        let db_map: DBMap<String, u64> = DBMap::new("db_map.db").unwrap();
+        db_map.insert("Test".to_string(), 42).unwrap();
+        db_map.insert("Hello".to_string(), 1).unwrap();
+        db_map.insert("World".to_string(), 1).unwrap();
+        assert_eq!(db_map.get("Test".to_string()).unwrap(), 42);
+        assert_eq!(db_map.get_keys(1).unwrap(), ["Hello".to_string(), "World".to_string()]);
+    }
+}
