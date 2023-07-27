84dffe6ebb8d333dd7a014360bc893a1b51dddf5
Teo <teo.orthlieb@gmail.com>
2023-07-27 16:18:52 +0200

    now supporting modal interaction responses

--- src/command_util.rs
+++ src/command_util.rs
@@ -5,6 +5,7 @@ use serenity::{
     model::{
         application::interaction::{
             InteractionResponseType,
+            modal::ModalSubmitInteraction,
             application_command::ApplicationCommandInteraction,
             message_component::MessageComponentInteraction
         },
@@ -65,6 +66,45 @@ impl CommandUtil for ApplicationCommandInteraction {
 
 #[async_trait]
 impl CommandUtil for MessageComponentInteraction {
+    async fn response(&self, http: &Http, message: MessageBuilder, kind: InteractionResponseType) -> Result<Message> {
+        (self
+            .create_interaction_response(http, |response| {
+                response
+                    .kind(kind)
+                    .interaction_response_data(|answer| {
+                        answer.allowed_mentions(|mentions| mentions.empty_users());
+                        answer.content(message.content);
+                        message.files.iter().for_each(|Attachment { file, filename }| {
+                            answer.add_file((file.as_slice(), filename.as_str()));
+                        });
+                        answer.set_buttons(message.buttons);
+                        answer
+                    })
+            }).await)
+        .context("Command create response failed")?;
+        Ok(self.get_interaction_response(http).await?)
+    }
+
+    async fn followup(&self, http: &Http, message: MessageBuilder) -> Result<()> {
+        (self
+            .create_followup_message(http, |answer| {
+                answer.allowed_mentions(|mentions| mentions.empty_users());
+                answer.content(message.content);
+                message.files.iter().for_each(|Attachment { file, filename }| {
+                    answer.add_file((file.as_slice(), filename.as_str()));
+                });
+                answer.set_buttons(message.buttons);
+                answer
+            })
+            .await)
+            .context("Command create followup failed")?;
+        Ok(())
+    }
+}
+
+
+#[async_trait]
+impl CommandUtil for ModalSubmitInteraction {
     async fn response(&self, http: &Http, message: MessageBuilder, kind: InteractionResponseType) -> Result<Message> {
         (self
             .create_interaction_response(http, |response| {
